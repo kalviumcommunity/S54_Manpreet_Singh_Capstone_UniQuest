ac40667ef2081ecbd090f94c692f22e2b75bcbb5
manpreetsingh04m <ms2404mk@gmail.com>
2024-03-13 14:47:33 +0530

    Basic server created for deployment

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+const express = require ('express')
+const app = express();
+const port = 8002;
+const mongoose = require("mongoose");
+require("dotenv").config()
+const cors = require('cors');
+
+async function main() {
+  await mongoose.connect(
+    process.env.MONGO_URL
+  );
+}
+app.use(express.json());
+app.use(cors())
+app.get('/',(req,res)=>{
+  main()
+  .then(() => {
+    res.send("Connection created Successfully!");
+  })
+  .catch((err) => {
+    res.send(err)
+  });
+})
+
+app.get('/ping',(req,res)=>{
+  res.send("pong")
+})
+app.listen(port,()=>{
+  console.log(`Server is started on ${port}`)
+})
