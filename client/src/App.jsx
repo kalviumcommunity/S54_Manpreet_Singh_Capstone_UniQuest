8207d0784849865e0a9563888327443e160524a4
manpreetsingh04m <ms2404mk@gmail.com>
2024-04-05 12:12:23 +0530

    Clerk Authorisation added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import ClerkAuth from './components/Clerk'
+// import SearchBar from './components/SearchBar'
 const App = () => {
   return (
     <div>
-      
+      <ClerkAuth/>
     </div>
   )
 }
