3fcedbfb1804253b7778c47fa39ad0d72ffd401d
Rikhil <rikhiltaneja@gmail.com>
2024-06-28 12:39:25 +0530

    sidebar component updated

---
+++ client/src/components/SidebarComponent.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import HomeIcon from '@mui/icons-material/Home';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import './Components.css';
+
+const SidebarComponent = () => {
+  return (
+    <Menu
+      customBurgerIcon={<MenuRoundedIcon />}
+      customCrossIcon={false}
+      width={'300px'}
+      styles={{
+        bmBurgerButton: {
+          position: 'fixed',
+          width: '36px',
+          height: '30px',
+          left: '36px',
+          top: '36px',
+          color: 'white'
+        },
+        bmBurgerBars: {
+          background: 'white'
+        },
+        bmMenuWrap: {
+          position: 'fixed',
+          height: '100%'
+        },
+        bmMenu: {
+          background: '#232323',
+          padding: '2vmax',
+          fontSize: '1.15em'
+        },
+        bmItemList: {
+          color: '#EEB572',
+          padding: '2em'
+        },
+        bmItem: {
+          display: 'flex',
+          color: 'white'
+        },
+        bmOverlay: {
+          background: 'none'
+        }
+      }}
+    >
+      <div className="menuItem">
+        <HomeIcon /> Home
+      </div>
+      <div className="menuItem">
+        <QuestionMarkIcon /> FAQ
+      </div>
+    </Menu>
+  );
+};
+
+export default SidebarComponent;
