91d2663106fc8c0652106a6f228c9d7dd5101c9c
aayushwrld <aayush.arora@kalvium.community>
2024-07-03 17:01:33 +0530

    Routes + Private Routing + Context Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,12 @@ import Navbar from './components/Navbar'
 import SearchBar from './components/SearchBar'
 import AboutUs from './components/AboutUs'
 import SearchPage from './components/SearchPage'
+import AllRoutes from './components/Routes/AllRoutes'
 // import Testimonials from './components/Testimonial'
 const App = () => {
   return (
     <div>
-      {/* <ClerkAuth/> */}
-      {/* <Homepage/> */}
-      {/* <Navbar/>  */}
-      {/* <SearchBar/> */}
-      {/* <Testimonials/> */}
-      <SearchPage/>
+      <AllRoutes />
     </div>
   )
 }
